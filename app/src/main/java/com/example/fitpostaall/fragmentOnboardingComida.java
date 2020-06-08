a435715f427dd20f2e2df89c7454cc14b9a1742a
fedek186 <fedek186@gmail.com>
2020-06-08 01:32:40 -0300

    lllllllllllllllll

--- app/src/main/java/com/example/fitpostaall/fragmentOnboardingComida.java
+++ app/src/main/java/com/example/fitpostaall/fragmentOnboardingComida.java
@@ -1,4 +1,29 @@
 package com.example.fitpostaall;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+public class fragmentOnboardingComida extends Fragment implements View.OnClickListener{
+    ImageView flechaI;
+    Button listo;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View vista;
+        vista = inflater.inflate(R.layout.layout_onboadingcomida, container, false);
+        listo= vista.findViewById(R.id.btnListoOn);
+        flechaI = vista.findViewById(R.id.imageViewFlechaIzqComida);
+        return vista;
+    }
 
-public class fragmentOnboardingComida {
+    public void onClick(View vista) {
+        Button botonApretado;
+        botonApretado = (Button) vista;
+        if (botonApretado.getId() == listo.getId()) {
+            MainActivity main = (MainActivity) getActivity();
+            main.pasarAPrin();
+        }
+    }
 }
--- app/src/main/java/com/example/fitpostaall/fragmentPaginaPrincipal.java
+++ app/src/main/java/com/example/fitpostaall/fragmentPaginaPrincipal.java
@@ -1,5 +1,17 @@
 package com.example.fitpostaall;
-
-public class fragmentPaginaPrincipal {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+public class fragmentPaginaPrincipal extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View vista;
+        vista = inflater.inflate(R.layout.layout_paginaprincipal, container, false);
+    return vista;
+    }
 
 }
