91d8dd7382c5808b6e98d3dd1d5e386b05817cd0
ilantrup <66500833+ilantrup@users.noreply.github.com>
2020-06-10 02:04:40 -0300

    Leer DB

--- app/src/main/java/com/example/fitpostaall/MainActivity.java
+++ app/src/main/java/com/example/fitpostaall/MainActivity.java
@@ -5,20 +5,34 @@ import android.os.Bundle;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends Activity {
     FragmentManager manager;
     FragmentTransaction transacFrag;
     BottomNavigationView nav;
+
+    //database
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         manager = getFragmentManager();
         Fragment fragIncio;
@@ -28,6 +42,23 @@ public class MainActivity extends Activity {
         transacFrag.commit();
         nav=findViewById(R.id.nav);
         nav.setOnNavigationItemSelectedListener(navMethod);
+
+        db.collection("usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
     }
    private  BottomNavigationView.OnNavigationItemSelectedListener navMethod= new BottomNavigationView.OnNavigationItemSelectedListener() {
        @Override
