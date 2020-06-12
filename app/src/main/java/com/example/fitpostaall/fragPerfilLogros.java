18d5e43d2ceac1a178f909978b8799970ee67433
fedek186 <fedek186@gmail.com>
2020-06-12 00:51:45 -0300

    NuevoLayoutLista

--- app/src/main/java/com/example/fitpostaall/fragPerfilLogros.java
+++ app/src/main/java/com/example/fitpostaall/fragPerfilLogros.java
@@ -1,4 +1,21 @@
 package com.example.fitpostaall;
 
-public class fragPerfilLogros {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+public class fragPerfilLogros extends Fragment {
+    ListView lista;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View vista;
+        vista = inflater.inflate(R.layout.layout_perfillogoros, container, false);
+        lista=vista.findViewById(R.id.lista);
+        return vista;
+    }
 }
