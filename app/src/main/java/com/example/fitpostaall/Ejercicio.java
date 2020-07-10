e2237d0d21bef8d293b2827b908f59f03ef1c64f
NICOLAS GOLDFARB <44362188@comunidadort.edu.ar>
2020-07-10 20:08:18 -0300

    Cerrar sesion

---
+++ app/src/main/java/com/example/fitpostaall/Ejercicio.java
@@ -0,0 +1,68 @@
+package com.example.fitpostaall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio {
+    private String _Destreza;
+    private Double _Dificultad;
+    private String _Foto;
+    private List<String> _Musculos;
+    private String _NombreEjercicio;
+
+    public void set_Foto(String _Foto) {
+        this._Foto = _Foto;
+    }
+
+    public void set_Destreza(String _Destreza) {
+        this._Destreza = _Destreza;
+    }
+
+    public void set_Dificultad(Double _Dificultad) {
+        this._Dificultad = _Dificultad;
+    }
+
+    public void set_Musculos(List<String> _Musculos) {
+        this._Musculos = _Musculos;
+    }
+
+    public void set_NombreEjercicio(String _NombreEjercicio) {
+        this._NombreEjercicio = _NombreEjercicio;
+    }
+
+
+
+
+
+    public Double get_Dificultad() {
+        return _Dificultad;
+    }
+
+    public List<String> get_Musculos() {
+        return _Musculos;
+    }
+
+    public String get_Destreza() {
+        return _Destreza;
+    }
+
+    public String get_Foto() {
+        return _Foto;
+    }
+
+    public String get_NombreEjercicio() {
+        return _NombreEjercicio;
+    }
+
+
+
+    public Ejercicio(){
+        this._Destreza = "";
+        this._Dificultad = 0.0;
+        this._Foto = "";
+        this._Musculos = new ArrayList<>();
+        this._NombreEjercicio = "";
+    }
+
+
+}
