a96ff3a4f4a75cdc7ef5c02896bc7e347959ed3e
FEDERICO ILAN KOZAK <44554540@comunidadort.edu.ar>
2020-07-23 20:02:33 -0300

    ComienzoCalend

--- app/src/main/java/com/example/fitpostaall/fragCalendario.java
+++ app/src/main/java/com/example/fitpostaall/fragCalendario.java
@@ -18,12 +18,27 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class fragCalendario extends Fragment {
+    com.applandeo.materialcalendarview.CalendarView calendarView;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View vista;
         vista=inflater.inflate(R.layout.layout_calendario,container,false);
+        calendarView = (CalendarView) vista.findViewById(R.id.calendarView);
+        List<EventDay> events = new ArrayList<>();
+        //Puedo añadir al dia de hoy un evento o una marca. Mientras mas grande sea el numero mas dias adelantes. Lo mismo al reves.
+        java.util.Calendar calendar1 = java.util.Calendar.getInstance();
+        calendar1.add(java.util.Calendar.DAY_OF_MONTH,1);
+        events.add(new EventDay(calendar1, R.drawable.circ_rutina));
+        calendarView.setEvents(events);
         return vista;
     }
+
 
 
 
