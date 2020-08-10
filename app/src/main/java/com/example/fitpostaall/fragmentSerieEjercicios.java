800c9c752aa48e5839beea59dcb785e9e189f083
NICOLAS GOLDFARB <44362188@comunidadort.edu.ar>
2020-08-09 23:32:58 -0300

    cronometro

--- app/src/main/java/com/example/fitpostaall/fragmentSerieEjercicios.java
+++ app/src/main/java/com/example/fitpostaall/fragmentSerieEjercicios.java
@@ -2,10 +2,14 @@ package com.example.fitpostaall;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,6 +23,13 @@ public class fragmentSerieEjercicios extends Fragment implements View.OnClickLis
     MainActivity main;
     ArrayList<Ejercicio> lisEj;
     int i;
+    Chronometer cronometro; // initiate a chronometer
+
+
+
+
+
+
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,9 +41,15 @@ public class fragmentSerieEjercicios extends Fragment implements View.OnClickLis
         txtI=vista.findViewById(R.id.cantI);
         txtN=vista.findViewById(R.id.nombEjer);
         imgE=vista.findViewById(R.id.imagenEj);
+        cronometro = (Chronometer) vista.findViewById(R.id.cronometro);
         sigui.setOnClickListener(this);
         lisEj=main.devolverArrayEj();
+        cronometro.setBase(SystemClock.elapsedRealtime());
         cargarDatos();
+
+
+
+
 
         return vista;
     }
@@ -46,6 +63,7 @@ public class fragmentSerieEjercicios extends Fragment implements View.OnClickLis
                 cargarDatos();
             }else {
                 txtN.setText("Finalizaste");
+                cronometro.stop();
             }
         }
 
@@ -56,5 +74,40 @@ public class fragmentSerieEjercicios extends Fragment implements View.OnClickLis
             txtI.setText((i+1)+"/"+lisEj.size());
             imgE.setImageDrawable(lisEj.get(i).get_Foto());
             txtN.setText(lisEj.get(i).get_NombreEjercicio());
+            cronometro.setBase(SystemClock.elapsedRealtime());
+            cronometro.start();
+            arrancarCuentaAtras();
+            i++;
+
+        }
+
+
+        public void arrancarCuentaAtras()
+        {
+            new CountDownTimer(11000, 1000){
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    cronometro.stop();
+
+
+
+                        if( i<lisEj.size())
+                        {
+                            cargarDatos();
+
+                        }else {
+                            txtN.setText("Finalizaste");
+                            cronometro.stop();
+                        }
+
+
+
+                }
+            }.start();
         }
 }
