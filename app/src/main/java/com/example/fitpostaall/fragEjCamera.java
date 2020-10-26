7ad20c5faf22da7fd8691bdeab510958e674bbaa
FEDERICO ILAN KOZAK <44554540@comunidadort.edu.ar>
2020-10-26 14:00:16 -0300

    Ides

--- app/src/main/java/com/example/fitpostaall/fragEjCamera.java
+++ app/src/main/java/com/example/fitpostaall/fragEjCamera.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class fragEjCamera  extends Fragment {
+    TextView txtTiempo,txtNombre;
+    ImageView imgE;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View vista;
         vista=inflater.inflate(R.layout.layout_ejcamara,container,false);
+        txtTiempo=vista.findViewById(R.id.cronometroEjCam);
+        txtNombre=vista.findViewById(R.id.cronometroEjCam);
+        imgE=vista.findViewById(R.id.imagenEjCam);
         return vista;
     }
+
 }
